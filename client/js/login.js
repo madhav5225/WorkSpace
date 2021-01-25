c59840c4458ca8ad1014c5ad5fcecf7f947c0e5f
Jatin Garg <jatin.manugarg@gmail.com>
2021-01-26 02:58:54 +0530

    registration added

--- client/js/login.js
+++ client/js/login.js
@@ -1,26 +1,24 @@
-function loginRequest()
-{
+function loginRequest() {
+
     //Retrieving Value
-    const email=document.getElementById('login-email').value;
-    const password=document.getElementById('login-password').value;
+    const email = $('#email_login').val();
+    const password = $('#password_login').val();
+
     // true if checkbox is checked
-    var rem_login='false';
-    if(document.getElementById('rem_login').checked)
-    {
-        rem_login='true';
-    }
-   // Making Post request 
-$.post("http://localhost:5000/login",
-        { email:""+email,
-        password:""+password,
-        rem_login:""+rem_login 
-        },
-        function(result){
-            if(result['success']==='true')
-            location.replace("http://localhost:5000/dashboard");
-            else
-           alert('Invalid Data:Try again');
+    var rem_login = ($('#rem_login').is(':checked') == "true") ? true : false;
+    console.log(rem_login);
+
+    $.post("/login", { email, password }, function (data) {
+        console.log(data + " :" + status);
+        if (data['msg'] === "success") {
+            window.location.href = '/dashboard';
         }
-    );
+        else {
+            alert(data['msg']);
+        }
+
+    }).fail(function (data) {
+        console.log("error: " + data);
+    });
 
 }
---
+++ client/js/register.js
@@ -0,0 +1,22 @@
+function registerRequest() {
+
+    //Retrieving Value
+    const email = $('#email_reg').val();
+    const password = $('#password_reg').val();
+    const name = $('#name_reg').val();
+
+
+    $.post("/register", { email, name, password }, function (data) {
+        console.log(data + " :" + status);
+        if (data['msg'] === "success") {
+            window.location.href = '/dashboard';
+        }
+        else {
+            alert(data['msg']);
+        }
+
+    }).fail(function (data) {
+        console.log("error: " + data);
+    });
+
+}
