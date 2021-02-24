674885a097265f6e3512359f1d5048e797576262
Madhav <666666madhav@gmail.com>
2021-02-24 23:34:23 +0530

    fixed buges in collab

--- client/js/collabration.js
+++ client/js/collabration.js
@@ -16,13 +16,13 @@ $('#collabrativeArea').keypress((e) => {
     
 })
 $('#collabrativeArea').keyup((e) => {
-    console.log(e.which);
-    console.log(  String.fromCharCode(e.charCode));
-    //position after key is pressed
-    currentUserCursor = $('#collabrativeArea').prop("selectionStart");
+    // console.log(e.which);
+    // console.log(  String.fromCharCode(e.charCode));
+    // //position after key is pressed
+     currentUserCursor = $('#collabrativeArea').prop("selectionStart");
       
-    console.log(currentUserCursor);
-    console.log(friendUserCursor);
+    // console.log(currentUserCursor);
+    // console.log(friendUserCursor);
     
     if (e.which == 37||e.which == 38||e.which == 39||e.which == 40 ) {
         socket.emit('Collabrative-Set-Pointer-from-Client',
@@ -32,18 +32,18 @@ $('#collabrativeArea').keyup((e) => {
 
 })
 $('#collabrativeArea').keydown((e) => {
-    console.log(e.which);
-    console.log(  String.fromCharCode(e.charCode));
-    //position before key is pressed
-    currentUserCursor = $('#collabrativeArea').prop("selectionStart");
+    // console.log(e.which);
+    // console.log(  String.fromCharCode(e.charCode));
+    // //position before key is pressed
+     currentUserCursor = $('#collabrativeArea').prop("selectionStart");
       
-    console.log(currentUserCursor);
-    console.log(friendUserCursor);
+    // console.log(currentUserCursor);
+    // console.log(friendUserCursor);
     
  if(e.which==8 && currentUserCursor!=0)
     {
         currentUserCursor--;
-        console.log('delete sent');
+       // console.log('delete sent');
         socket.emit('Collabrative-Insert-from-Client',
         { characterCode:e.which, 'friendUserId': friendUser.id,currentUserCursor });
         if (friendUserCursor > currentUserCursor)
@@ -54,7 +54,7 @@ $('#collabrativeArea').keydown((e) => {
 $('#collabrativeArea').click( e => {
 
     currentUserCursor = document.getElementById('collabrativeArea').selectionStart; 
-    console.log(currentUserCursor);
+  //  console.log(currentUserCursor);
     socket.emit('Collabrative-Set-Pointer-from-Client',
         { currentUserCursor, 'friendUserId': friendUser.id });
 
