25c2f1ec0061ee5cbe5327add3474e6c97d86403
Madhav <666666madhav@gmail.com>
2021-02-27 23:38:20 +0530

    fixed some bugs

--- client/js/checkHttpProtocol.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function () {
-//     //console.log('hey')
-//     // if (location.protocol !== 'https:') {
-//     //     location.replace(`https:${location.href.substring(location.protocol.length)}`);
-//      //}
-// });
