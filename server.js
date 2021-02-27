801b7564c2308b292ad9b215c8b691f3ed704b89
Madhav <666666madhav@gmail.com>
2021-02-27 19:17:31 +0530

    http request on heroku server will be redirected to https now

--- server.js
+++ server.js
@@ -24,28 +24,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // initialize cookie-parser to allow us access the cookies stored in the browser.
 app.use(cookieParser('secret'));
 
-// initialize express-session to allow us track the logged-in user across sessions.
-// app.use(function(req, res) {
-    
-//     res.redirect("http://" + req.headers.host + req.url);
-// });
+// initialize express-session to allow us track the logged-in user across sessions.  
+require('./session')(app);
+//redirects to https when hosted on heroku server
 var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
+    if (req.headers['x-forwarded-proto'] !== 'https'&& "production" === process.env.NODE_ENV) {
         return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     return next();
  };
-
-//  app.configure(function () {      
-//     if (env === 'production') {
-        
-//     }
-    // other configurations etc for express go here...
- //});
  app.use(forceSsl);
-   
-require('./session')(app);
-
+ 
 const server = require('./server/sockets/socketing.js')(app);
 const route = require('./server/routes/routes.js');
 
